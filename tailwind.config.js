860399eebbbc788dad7b9e0710a4f8d0bf8d5a09
Kabir Asani <iamkabirasani@gmail.com>
2024-02-01 01:14:55 +0530

    Install Daisy UI + custom themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,5 +14,35 @@ export default {
 			xl: '1280px'
 		}
 	},
-	plugins: []
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: [
+			{
+				chaos: {
+					primary: '#2797FE',
+					'primary-content': '#fff',
+					secondary: '#A971FF',
+					'secondary-content': '#fff',
+					accent: '#43B9B9',
+					'accent-content': '#fff',
+					neutral: '#9C9C9C',
+					'base-100': '#02040A',
+					'base-200': '#0E1117',
+					'base-300': '#161B22',
+					'base-content': '#fff'
+				}
+			},
+			{
+				light: {
+					...require('daisyui/src/theming/themes')['light'],
+					primary: '#2797FE',
+					'primary-content': '#fff',
+					secondary: '#A971FF',
+					'secondary-content': '#fff',
+					accent: '#43B9B9',
+					'accent-content': '#fff'
+				}
+			}
+		]
+	}
 };
