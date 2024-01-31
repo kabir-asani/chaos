0ed0d94d2397fedd40ce864e48fc56270367feb2
Kabir Asani <iamkabirasani@gmail.com>
2024-02-01 01:16:48 +0530

    Create lib/assets folder with some svg assets

---
+++ src/lib/assets/svg/CopyAfterIcon.svelte
@@ -0,0 +1,15 @@
+<svg
+	class={$$restProps.class || ''}
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:xlink="http://www.w3.org/1999/xlink"
+	viewBox="0,0,24,24"
+>
+	<path
+		fill="currentColor"
+		d="M14 8H4c-1.103 0-2 .897-2 2v10c0 1.103.897 2 2 2h10c1.103 0 2-.897 2-2V10c0-1.103-.897-2-2-2"
+	/>
+	<path
+		fill="currentColor"
+		d="M20 2H10a2 2 0 0 0-2 2v2h8a2 2 0 0 1 2 2v8h2a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2"
+	/>
+</svg>
