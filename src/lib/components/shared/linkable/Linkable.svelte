b61f86600b66d30c9d1961d5e14c60f5a17d4302
Kabir Asani <iamkabirasani@gmail.com>
2024-02-01 01:18:36 +0530

    Create Linkable.svelte

---
+++ src/lib/components/shared/linkable/Linkable.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let href: String;
+	export let external: Boolean = true;
+</script>
+
+<a
+	href={href.valueOf()}
+	target={external ? '_blank' : undefined}
+	rel={external ? 'noopener noreferrer' : undefined}
+	class="hover:opacity-60"
+>
+	<slot />
+</a>
