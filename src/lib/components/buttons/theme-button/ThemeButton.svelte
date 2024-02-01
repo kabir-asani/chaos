df2a8fc58fa0f15a36b34c7268962bcdd8fe1533
Kabir Asani <iamkabirasani@gmail.com>
2024-02-02 01:48:17 +0530

    Create ThemeButton.svelte

---
+++ src/lib/components/buttons/theme-button/ThemeButton.svelte
@@ -0,0 +1,12 @@
+<script>
+	import MoonIcon from '$lib/assets/svg/MoonIcon.svelte';
+	import SunIcon from '$lib/assets/svg/SunIcon.svelte';
+</script>
+
+<label class="swap swap-rotate">
+	<input type="checkbox" class="theme-controller" value="light" />
+
+	<MoonIcon class="swap-on fill-current h-4 w-4" />
+
+	<SunIcon class="swap-off fill-current h-4 w-4" />
+</label>
