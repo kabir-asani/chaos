b13b1d2d252fc1825047214d8c39ea3e3ff0b252
Kabir Asani <iamkabirasani@gmail.com>
2024-02-02 00:52:56 +0530

    Update Entrepreneur.svelte

--- src/lib/routes/home/entrepreneur/Entrepreneur.svelte
+++ src/lib/routes/home/entrepreneur/Entrepreneur.svelte
@@ -1,1 +1,26 @@
-<div class="h-12 bg-amber-400" />
+<script>
+	import SectionItem from '$lib/components/shared/section/section-item/SectionItem.svelte';
+	import SectionTitle from '$lib/components/shared/section/section-title/SectionTitle.svelte';
+	import SectionContent from '$lib/components/shared/section/section-content/SectionContent.svelte';
+	import EntrepreneurshipItem from './components/EntrepreneurItem.svelte';
+</script>
+
+<SectionItem id="entrepreneurship">
+	<SectionTitle slot="title" text="Entrepreneur" />
+	<SectionContent slot="content">
+		<EntrepreneurshipItem
+			role="Founder & CEO"
+			company="Sarcasm Studio"
+			period="2024 - Present"
+			onelineDescription="Leading content creation efforts across channels with an aim to better people's life with qualitatively-dense content"
+		/>
+
+		<EntrepreneurshipItem
+			role="Co-Founder"
+			company="Async Bay"
+			companyUrl="https://asyncbay.com"
+			period="2023 - Present"
+			onelineDescription="Leading a software studio being run ground up by engineers (frontend & backend) with over 5+ years of experience each"
+		/>
+	</SectionContent>
+</SectionItem>
