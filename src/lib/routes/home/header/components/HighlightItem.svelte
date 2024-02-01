ce39cf8983e8d5fd99e6e59715567d0147ecd610
Kabir Asani <iamkabirasani@gmail.com>
2024-02-01 19:41:43 +0530

    Create HighlightItem.svelte

---
+++ src/lib/routes/home/header/components/HighlightItem.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	export let text: String;
+	export let sectionId: String;
+
+	const scrollToSection = (event: MouseEvent): void => {
+		event.preventDefault();
+		const targetId = (event.currentTarget as HTMLAnchorElement).getAttribute('href');
+
+		console.log(targetId);
+
+		if (targetId) {
+			const targetElement = document.querySelector(targetId);
+			console.log(targetElement);
+			if (targetElement) {
+				targetElement.scrollIntoView({ behavior: 'smooth', inline: 'end' });
+			}
+		}
+	};
+</script>
+
+<a href="#{sectionId.valueOf()}" on:click={scrollToSection}>
+	<div class="bg-base-100 rounded-lg border border-primary px-2 py-1 text-sm text-primary">
+		{text}
+	</div>
+</a>
