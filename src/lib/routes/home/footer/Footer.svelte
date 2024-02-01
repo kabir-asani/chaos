919d9a7afe1b94456f6420cebc3cb4fa97d8f774
Kabir Asani <iamkabirasani@gmail.com>
2024-02-02 01:48:19 +0530

    Update Footer.svelte

--- src/lib/routes/home/footer/Footer.svelte
+++ src/lib/routes/home/footer/Footer.svelte
@@ -1,1 +1,27 @@
-<div class="h-12 bg-amber-400" />
+<script>
+	import GitHubIcon from '$lib/assets/svg/GitHubIcon.svelte';
+	import InstagramIcon from '$lib/assets/svg/InstagramIcon.svelte';
+	import XIcon from '$lib/assets/svg/XIcon.svelte';
+	import Clickable from '$lib/components/linkable/Linkable.svelte';
+	import ThemeButton from '$lib/components/buttons/theme-button/ThemeButton.svelte';
+</script>
+
+<footer class="footer items-center p-4 text-base-content bg-base-200">
+	<aside class="items-center grid-flow-col">
+		<p>Copyright © 2024 - All rights reserved</p>
+	</aside>
+	<nav class="grid-flow-col gap-4 items-center md:place-self-center md:justify-self-end">
+		<ThemeButton />
+		<Clickable href="https://instagram.com/kabir.asani">
+			<InstagramIcon class="h-5 w-5 fill-base-content" />
+		</Clickable>
+
+		<Clickable href="https://x.com/KabirAsani">
+			<XIcon class="h-5 w-5 fill-base-content" />
+		</Clickable>
+
+		<Clickable href="https://github.com/kabir-asani">
+			<GitHubIcon class="h-5 w-5 fill-base-content" />
+		</Clickable>
+	</nav>
+</footer>
