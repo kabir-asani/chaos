dca83246aade51b3fa8ff6121292b66eb6b73cdc
Kabir Asani <iamkabirasani@gmail.com>
2024-02-02 01:27:50 +0530

    Create CreatorItem.svelte

---
+++ src/lib/routes/home/creator/components/CreatorItem.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let text: String;
+	export let tag: String;
+	export let url: String;
+
+	import Linkable from '$lib/components/shared/linkable/Linkable.svelte';
+</script>
+
+<div class="row gap-4 items-center">
+	<div class="col gap-2 items-start">
+		<Linkable href={url.valueOf()}>
+			<div class="text-primary">{text}</div>
+		</Linkable>
+
+		<div class="text-neutral">{tag}</div>
+	</div>
+
+	<div class="grow border-neutral-light" />
+
+	<div class="h-5 w-5 fill-base-content">
+		<slot />
+	</div>
+</div>
