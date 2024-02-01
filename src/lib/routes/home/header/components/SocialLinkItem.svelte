92d62978ad279d608432c3f5ff6b4d10308fc036
Kabir Asani <iamkabirasani@gmail.com>
2024-02-02 00:08:33 +0530

    Update SocialLinkItem.svelte

--- src/lib/routes/home/header/components/SocialLinkItem.svelte
+++ src/lib/routes/home/header/components/SocialLinkItem.svelte
@@ -2,14 +2,15 @@
 	export let text: String;
 	export let url: String;
 
+	import Row from '$lib/components/shared/layouts/row/Row.svelte';
 	import Linkable from '$lib/components/shared/linkable/Linkable.svelte';
 </script>
 
-<Linkable href={url.valueOf()}>
-	<div class="row items-center gap-2">
+<Linkable href={url.valueOf()} class="font-medium">
+	<Row class="items-center gap-2">
 		<div class="h-54 w-5 fill-base-content">
 			<slot />
 		</div>
 		{text}
-	</div>
+	</Row>
 </Linkable>
