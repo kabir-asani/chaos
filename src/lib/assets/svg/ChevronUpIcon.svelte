7e64489168467b204439767e0bded274d906a4ec
Kabir Asani <iamkabirasani@gmail.com>
2024-02-02 03:16:46 +0530

    Create ChevronUpIcon.svelte

---
+++ src/lib/assets/svg/ChevronUpIcon.svelte
@@ -0,0 +1,17 @@
+<svg
+	class={$$restProps.class || ''}
+	xmlns="http://www.w3.org/2000/svg"
+	xmlns:xlink="http://www.w3.org/1999/xlink"
+	viewBox="0 0 24 24"
+>
+	<g
+		fill="none"
+		stroke="currentColor"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		stroke-width="2"
+	>
+		<path d="m9 13l3-3l3 3" />
+		<path d="M12 3c7.2 0 9 1.8 9 9s-1.8 9-9 9s-9-1.8-9-9s1.8-9 9-9" />
+	</g>
+</svg>
