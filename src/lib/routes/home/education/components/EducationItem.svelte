c7ef9d6eb0979dd5236748173221958b1e2be833
Kabir Asani <iamkabirasani@gmail.com>
2024-02-02 01:30:52 +0530

    Create EducationItem.svelte

---
+++ src/lib/routes/home/education/components/EducationItem.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	export let degree: String;
+	export let field: String;
+	export let institution: String;
+	export let period: String;
+	export let score: String;
+</script>
+
+<div class="card base-base-200 border-neutral-light shadow-2xl md:sticky-10">
+	<div class="card-body">
+		<div class="col md:row gap-2 items-start md:items-center">
+			<h1 class="text-xl">
+				{degree}
+			</h1>
+
+			<div class="wrap items-center gap-2">
+				<div class="badge badge-neutral text-sm">
+					{field}
+				</div>
+
+				<div class="badge badge-neutral text-sm">
+					{period}
+				</div>
+			</div>
+		</div>
+
+		<div class="text-neutral">
+			{institution}
+		</div>
+
+		<p class="text-neutral">{score}</p>
+	</div>
+</div>
