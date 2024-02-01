0abed33864e69c4b538c4ca7247bdef8b3724e01
Kabir Asani <iamkabirasani@gmail.com>
2024-02-02 01:30:55 +0530

    Update Education.svelte

--- src/lib/routes/home/education/Education.svelte
+++ src/lib/routes/home/education/Education.svelte
@@ -1,1 +1,19 @@
-<div class="h-12 bg-amber-400" />
+<script>
+	import SectionItem from '$lib/components/shared/section/section-item/SectionItem.svelte';
+	import SectionTitle from '$lib/components/shared/section/section-title/SectionTitle.svelte';
+	import SectionContent from '$lib/components/shared/section/section-content/SectionContent.svelte';
+	import EducationItem from './components/EducationItem.svelte';
+</script>
+
+<SectionItem id="education">
+	<SectionTitle slot="title" text="Education" />
+	<SectionContent slot="content">
+		<EducationItem
+			degree="Bachelor of Engineering"
+			field="Computer Science"
+			institution="Siddaganga Institute Of Technology"
+			period="2016-2020"
+			score="9.32 CGPA"
+		/>
+	</SectionContent>
+</SectionItem>
