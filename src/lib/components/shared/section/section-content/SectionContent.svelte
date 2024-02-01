fea4e2a9bf0abde47a68fdc65ec7c61b7241c2c6
Kabir Asani <iamkabirasani@gmail.com>
2024-02-02 00:28:37 +0530

    Update SectionContent.svelte

--- src/lib/components/shared/section/section-content/SectionContent.svelte
+++ src/lib/components/shared/section/section-content/SectionContent.svelte
@@ -1,11 +1,23 @@
-{#if $$slots.grid}
+<script context="module" lang="ts">
+	export type Variant = 'grid' | 'column' | 'sole';
+</script>
+
+<script lang="ts">
+	export let variant: Variant = 'column';
+</script>
+
+{#if variant === 'grid'}
 	<div class="grow col md:grid-col-2 gap-4">
-		<slot name="grid" />
+		<slot />
 	</div>
 {/if}
 
-{#if $$slots.column}
-	<div class={'grow col gap-4'}>
-		<slot name="column" />
+{#if variant === 'column'}
+	<div class="grow col gap-4">
+		<slot />
 	</div>
+{/if}
+
+{#if variant === 'sole'}
+	<slot />
 {/if}
