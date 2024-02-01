66a9021ed7b7e03b9105b3953c0df7c53870f3d5
Kabir Asani <iamkabirasani@gmail.com>
2024-02-01 19:41:47 +0530

    Create SocialLinkItem.svelte

---
+++ src/lib/routes/home/header/components/SocialLinkItem.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let text: String;
+	export let url: String;
+
+	import Linkable from '$lib/components/shared/linkable/Linkable.svelte';
+</script>
+
+<Linkable href={url.valueOf()}>
+	<div class="row items-center gap-2">
+		<div class="h-54 w-5 fill-base-content">
+			<slot />
+		</div>
+		{text}
+	</div>
+</Linkable>
