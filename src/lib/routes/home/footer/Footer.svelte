a8416b2d57c0ecab56eeaf19446c445d68c07756
Kabir Asani <iamkabirasani@gmail.com>
2024-02-02 05:07:05 +0530

    Update Footer.svelte

--- src/lib/routes/home/footer/Footer.svelte
+++ src/lib/routes/home/footer/Footer.svelte
@@ -2,7 +2,7 @@
 	import GitHubIcon from '$lib/assets/svg/GitHubIcon.svelte';
 	import InstagramIcon from '$lib/assets/svg/InstagramIcon.svelte';
 	import XIcon from '$lib/assets/svg/XIcon.svelte';
-	import Clickable from '$lib/components/linkable/Linkable.svelte';
+	import Linkable from '$lib/components/linkable/Linkable.svelte';
 	import ThemeButton from '$lib/components/buttons/theme-button/ThemeButton.svelte';
 	import RedditIcon from '$lib/assets/svg/RedditIcon.svelte';
 </script>
@@ -11,22 +11,27 @@
 	<aside class="items-center grid-flow-col">
 		<p>Copyright © 2024 - All rights reserved by Kabir Asani</p>
 	</aside>
+	<nav class="grid-flow-col gap-4 items-center">
+		<Linkable href="/attributions" class="link" external={false}>Attributions</Linkable>
+		<Linkable href="/affiliates" class="link" external={false}>Affiliates</Linkable>
+		<Linkable href="/shout-outs" class="link" external={false}>Shout Outs</Linkable>
+	</nav>
 	<nav class="grid-flow-col gap-4 items-center md:place-self-center md:justify-self-end">
 		<ThemeButton />
-		<Clickable href="https://instagram.com/kabir.asani">
+		<Linkable href="https://instagram.com/kabir.asani">
 			<InstagramIcon class="h-5 w-5 fill-base-content" />
-		</Clickable>
+		</Linkable>
 
-		<Clickable href="https://x.com/KabirAsani">
+		<Linkable href="https://x.com/KabirAsani">
 			<XIcon class="h-5 w-5 fill-base-content" />
-		</Clickable>
+		</Linkable>
 
-		<Clickable href="https://github.com/kabir-asani">
+		<Linkable href="https://github.com/kabir-asani">
 			<GitHubIcon class="h-5 w-5 fill-base-content" />
-		</Clickable>
+		</Linkable>
 
-		<Clickable href="https://reddit.com/u/kabirasani">
+		<Linkable href="https://reddit.com/u/kabirasani">
 			<RedditIcon class="h-5 w-5 fill-base-content" />
-		</Clickable>
+		</Linkable>
 	</nav>
 </footer>
