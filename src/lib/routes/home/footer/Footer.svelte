3ce5a041f0d4447a8012e262b607ff39c64c8e5e
Kabir Asani <iamkabirasani@gmail.com>
2024-02-02 02:02:25 +0530

    Update Footer.svelte

--- src/lib/routes/home/footer/Footer.svelte
+++ src/lib/routes/home/footer/Footer.svelte
@@ -4,11 +4,12 @@
 	import XIcon from '$lib/assets/svg/XIcon.svelte';
 	import Clickable from '$lib/components/linkable/Linkable.svelte';
 	import ThemeButton from '$lib/components/buttons/theme-button/ThemeButton.svelte';
+	import RedditIcon from '$lib/assets/svg/RedditIcon.svelte';
 </script>
 
 <footer class="footer items-center p-4 text-base-content bg-base-200">
 	<aside class="items-center grid-flow-col">
-		<p>Copyright © 2024 - All rights reserved</p>
+		<p>Copyright © 2024 - All rights reserved by Kabir Asani</p>
 	</aside>
 	<nav class="grid-flow-col gap-4 items-center md:place-self-center md:justify-self-end">
 		<ThemeButton />
@@ -22,6 +23,10 @@
 
 		<Clickable href="https://github.com/kabir-asani">
 			<GitHubIcon class="h-5 w-5 fill-base-content" />
+		</Clickable>
+
+		<Clickable href="https://reddit.com/u/kabirasani">
+			<RedditIcon class="h-5 w-5 fill-base-content" />
 		</Clickable>
 	</nav>
 </footer>
