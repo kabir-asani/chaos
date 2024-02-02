0777e227a0fadeb83a5e8f2e95e5c6ff0455bb06
Kabir Asani <iamkabirasani@gmail.com>
2024-02-03 00:33:54 +0530

    Update +page.svelte

--- src/routes/shout-outs/+page.svelte
+++ src/routes/shout-outs/+page.svelte
@@ -1,3 +1,28 @@
-<div class="col gap-16 responsive-x-container my-16">
+<script>
+	import ShotOutItem from './components/shot-out-item/ShotOutItem.svelte';
+</script>
+
+<div class="col gap-4 responsive-x-container my-16">
+	<div class="text-sm breadcrumbs">
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/shout-outs">Shout Outs</a></li>
+		</ul>
+	</div>
+
 	<h1 class="text-xl">Shout Outs</h1>
+
+	<div class="col gap-4 items-stretch">
+		<ShotOutItem receiver="Svelte" receiverLink="https://svelte.dev" />
+		<ShotOutItem receiver="SvelteKit" receiverLink="https://kit.svelte.dev" />
+		<ShotOutItem receiver="Tailwind CSS" receiverLink="https://tailwindcss.com" />
+		<ShotOutItem receiver="Daisy UI" receiverLink="https://daisyui.com" />
+		<ShotOutItem receiver="Vite" receiverLink="https://vitejs.dev" />
+		<ShotOutItem receiver="Vercel" receiverLink="https://vercel.com" />
+		<ShotOutItem receiver="Prettier" receiverLink="https://prettier.io" />
+		<ShotOutItem receiver="ES Lint" receiverLink="https://eslint.org" />
+		<ShotOutItem receiver="VS Code" receiverLink="https://code.visualstudio.com" />
+		<ShotOutItem receiver="Git" receiverLink="https://git-scm.com" />
+		<ShotOutItem receiver="GitHub" receiverLink="https://github.com" />
+	</div>
 </div>
