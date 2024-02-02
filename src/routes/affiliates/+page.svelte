d0e0b3fb2a8c347b6a2122443482ee58ccb7a143
Kabir Asani <iamkabirasani@gmail.com>
2024-02-03 00:35:14 +0530

    Update /affiliates

--- src/routes/affiliates/+page.svelte
+++ src/routes/affiliates/+page.svelte
@@ -1,3 +1,10 @@
-<div class="col gap-16 responsive-x-container my-16">
+<div class="col gap-4 responsive-x-container my-16">
+	<div class="text-sm breadcrumbs">
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/affiliates">Affiliates</a></li>
+		</ul>
+	</div>
+
 	<h1 class="text-xl">Affiliates</h1>
 </div>
