8fee5933e08fe68d7d10113e3accc6aa4f6f1e64
Kabir Asani <iamkabirasani@gmail.com>
2024-02-03 00:33:52 +0530

    Create ShotOutItem.svelte

---
+++ src/routes/shout-outs/components/shot-out-item/ShotOutItem.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let receiver: String;
+	export let receiverLink: String;
+
+	import ChevronRightIcon from '$lib/assets/svg/ChevronRightIcon.svelte';
+	import Linkable from '$lib/components/linkable/Linkable.svelte';
+</script>
+
+<div class="row gap-4 items-center">
+	<ChevronRightIcon class="fill-neutral h-4 w-4" />
+	<Linkable href={receiverLink.valueOf()}>
+		<div class="text-primary">{receiver}</div>
+	</Linkable>
+</div>
