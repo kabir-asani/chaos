a47730362893cb2a4a375dbbeab5f0bbf83ca18b
Kabir Asani <iamkabirasani@gmail.com>
2024-02-02 15:06:02 +0530

    Update profile-picture

--- src/lib/routes/home/header/Header.svelte
+++ src/lib/routes/home/header/Header.svelte
@@ -10,7 +10,7 @@
 <div class="col md:row gap-4 items-stretch md:items-center">
 	<div class="avatar self-center md:w-36">
 		<div class="w-32 rounded-full">
-			<img src="/images/profile-picture.jpg" alt="Kabir Asani's close up shot" />
+			<img src="/images/profile-picture.png" alt="Kabir Asani's close up shot" />
 		</div>
 	</div>
 
