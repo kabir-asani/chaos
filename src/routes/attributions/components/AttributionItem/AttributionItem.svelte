b4c5577b393c3c14473aed14ecf153238fb89b38
Kabir Asani <iamkabirasani@gmail.com>
2024-02-03 00:15:51 +0530

    Create AttributionItem.svelte

---
+++ src/routes/attributions/components/AttributionItem/AttributionItem.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let resource: String;
+	export let resourceLink: String;
+	export let provider: String;
+	export let providerLink: String;
+
+	import Linkable from '$lib/components/linkable/Linkable.svelte';
+</script>
+
+<div class={'row gap-4 items-center' + ' ' + ($$restProps.class || '')}>
+	<div class="h-5 w-5 fill-base-content">
+		<slot />
+	</div>
+
+	<div class="row gap-1 items-center">
+		<Linkable href={resourceLink.valueOf()}>
+			<div class="text-primary">{resource}</div>
+		</Linkable>
+
+		<div class="text-base-content">by</div>
+
+		<Linkable href={providerLink.valueOf()}>
+			<div class="text-primary">{provider}</div>
+		</Linkable>
+	</div>
+</div>
