51719ba784c15979f9729cd48f990013677ff226
Kabir Asani <iamkabirasani@gmail.com>
2024-02-14 21:43:55 +0530

    Update CreatorItem.svelte

--- src/routes/components/creator/components/CreatorItem.svelte
+++ src/routes/components/creator/components/CreatorItem.svelte
@@ -6,18 +6,20 @@
 	import Linkable from '$lib/components/linkable/Linkable.svelte';
 </script>
 
-<div class={'row gap-4 items-center' + ' ' + ($$restProps.class || '')}>
-	<div class="col gap-2 items-start">
-		<Linkable href={url.valueOf()}>
-			<div class="text-primary">{text}</div>
-		</Linkable>
+<div class="col gap-2">
+	<div class={'row gap-4 items-center' + ' ' + ($$restProps.class || '')}>
+		<div class="col gap-2 items-start">
+			<Linkable href={url.valueOf()}>
+				<div class="text-primary">{text}</div>
+			</Linkable>
+		</div>
 
-		<div class="text-neutral">{tag}</div>
+		<div class="grow border-neutral-light" />
+
+		<div class="h-5 w-5 fill-base-content">
+			<slot />
+		</div>
 	</div>
 
-	<div class="grow border-neutral-light" />
-
-	<div class="h-5 w-5 fill-base-content">
-		<slot />
-	</div>
+	<div class="text-neutral">{tag}</div>
 </div>
