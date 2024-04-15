e242205e4f342c7ada0a2fecf1c9ebc7793c9a14
Kabir Asani <iamkabirasani@gmail.com>
2024-04-15 11:00:37 +0530

    Create Advertisement.svelte

---
+++ src/routes/components/advertisement/Avertisement.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Linkable from '$lib/components/linkable/Linkable.svelte';
+</script>
+
+<Linkable external={true} href="https://discord.gg/4VZ5P7WJ">
+	<div class="flex flex-row gap-2 justify-center items-center p-4 bg-base-300">
+		✨✨✨
+		<div class="underline text-center">Join KStack Discord Server (Kabir Asani's Community)</div>
+		✨✨✨
+	</div>
+</Linkable>
