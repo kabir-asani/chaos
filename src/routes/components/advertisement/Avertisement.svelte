6ae7d304a56e9e975c107bfb89a551a310bd9762
Kabir Asani <iamkabirasani@gmail.com>
2024-04-22 09:57:45 +0530

    Update Advertisement.svelte with correct link

--- src/routes/components/advertisement/Avertisement.svelte
+++ src/routes/components/advertisement/Avertisement.svelte
@@ -2,7 +2,7 @@
 	import Linkable from '$lib/components/linkable/Linkable.svelte';
 </script>
 
-<Linkable external={true} href="https://discord.gg/4VZ5P7WJ">
+<Linkable external={true} href="https://chat.whatsapp.com/HJUZvAVyj7R3lZx6BW8bsw">
 	<div class="flex flex-row gap-2 justify-center items-center p-4 bg-base-300">
 		✨✨✨
 		<div class="underline text-center">Join KStack On WhatsApp (Kabir Asani's Community)</div>
