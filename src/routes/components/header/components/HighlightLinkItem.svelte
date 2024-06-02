0924b388f85c89a3ae73649f404e7d4d740f39f3
Kabir Asani <iamkabirasani@gmail.com>
2024-06-03 02:50:22 +0530

    Create HighlightLinkItem.svelte

---
+++ src/routes/components/header/components/HighlightLinkItem.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let text: String;
+	export let url: String;
+</script>
+
+<a href={url.valueOf()}>
+	<div
+		class="bg-primary rounded-lg border border-primary px-2 py-1 text-sm font-medium hover:opacity-75"
+	>
+		{text}
+	</div>
+</a>
